a064ac13edaf65a155c98787129614e96848eea9
Jonathan Brownlee <jonathanbrownlee@knights.ucf.edu>
2021-02-15 08:31:06 -0500

    Fixed invalid syntax

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -1,6 +1,7 @@
 import os
 from aiohttp import web
-import gidgethub import routing, sansio
+import gidgethub
+import routing, sansio
 from gidgethub import aiohttp as gh_aiohttp
 
 routes = web.RouteTableDef()
@@ -11,8 +12,7 @@ async def issue_opened_event(event, gh, *args, **kwargs):
     url = event.data["issue"]["comments_url"]
     author = event.data["issue"]["user"]["login"]
 
-    message = f"Thanks for the report @{author}! I will look into it\
-     ASAP! (I'm a bot)."
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
     await gh.post(url, data = {"body": message})
 @routes.post("/")
 async def main(request):
